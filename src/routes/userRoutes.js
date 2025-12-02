c6cc0224a48be584253b28426f184344ee14a1d0
GUILHERME LIRIOBERTO DA SILVA ALVES <99543451+Liriogui@users.noreply.github.com>
2025-12-02 12:18:34 -0400

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const ctrl = require('../controllers/userController');
+const isAuth = require('../middlewares/auth');
 
-const ctrl = require("../controllers/userController");
-const isAuth = require("../middlewares/auth");
-
-router.get("/login", ctrl.loginPage);
-router.post("/login", ctrl.login);
-
-router.get("/register", ctrl.registerPage);
-router.post("/register", ctrl.register);
-
-router.get("/perfil", isAuth, ctrl.perfil);
-
-router.get("/logout", ctrl.logout);
-
+router.get('/register', ctrl.registerPage);
+router.post('/register', ctrl.register); // protegido por CSRF
+router.get('/perfil', isAuth, ctrl.perfil);
+router.get('/logout', ctrl.logout);
 module.exports = router;
